70339b02279be5155d76647a9c0d2665e614f2e3
VishwajeethV <spacexblue80@gmail.com>
2022-08-14 05:32:23 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function cap() {
+    window.location.href = "https://vishwajeethv.github.io/Feedback-for-independence-filter/";
+}
+
+function mask() {
+    window.location.href = "https://vishwajeethv.github.io/Independance-filter-mask/";
+}
+
+function feedback() {
+    window.location.href = "https://vishwajeethv.github.io/Feedback-for-independence-filter/";
+}
