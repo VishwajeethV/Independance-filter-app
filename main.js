b434418f02e1f86a12cd4c3fc288c6e2be614c2b
VishwajeethV <spacexblue80@gmail.com>
2022-08-14 05:34:54 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function cap() {
-    window.location.href = "https://vishwajeethv.github.io/Feedback-for-independence-filter/";
+    window.location.href = "https://vishwajeethv.github.io/Independance-filter-cap/";
 }
 
 function mask() {
